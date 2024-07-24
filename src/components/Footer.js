fffc71d16cf90abac77147b824ae1906fd304bbd
mehdi-khallaghi <mkh.computer@gmail.com>
2024-07-24 17:15:01 +0330

    no.1

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import Media from "./Media";
+
+function Footer (){
+    return (
+        <div className="footer">
+            <Media />
+            <hr />
+            <h2>This Is Site Footer</h2>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../css/Header.css'
+
+function Header(){
+    return(
+        <div className="header">
+            <h2 id="title">I am header section </h2>
+        </div>
+    )
+}
+
+export default Header;
