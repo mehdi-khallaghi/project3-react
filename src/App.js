f646a20d2e0c8c1d804c3af43108918433f2ddb7
mehdi-khallaghi <mkh.computer@gmail.com>
2024-07-27 12:54:46 +0330

    start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from './Header/Header';
-import Article from './Article/Article';
-import Footer from './Footer/Footer';
+
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <Article />
-      <Footer />
+     
     </div>
   );
 }
